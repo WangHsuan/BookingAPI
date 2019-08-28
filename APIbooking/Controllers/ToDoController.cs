0a8fcbc07aaf07dd90114ef9607661f1288d338a
Hsuan Wang <n10105981@qut.edu.au>
2019-08-29 09:34:02 +1000

    the functionality of fetch microsoft outlook api has done

--- APIbooking/Controllers/ToDoController.cs
+++ APIbooking/Controllers/ToDoController.cs
@@ -20,6 +20,7 @@ using System.Text;
 using System.IO;
 using System.Net;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace APIbooking.Controllers
 {
@@ -31,113 +32,57 @@ namespace APIbooking.Controllers
 
         //HttpWebResponse response;
 
-        //[EnableCors(origins: "http://localhost:61159", headers: "*", methods: "*")]
-        public static HttpResponseMessage getAccessToken() {
-
-            //first bad request----------------------------------------------------
-            HttpClient client = new HttpClient();
-
-            // 將 data 轉為 json
-
-            string body = "client_id=7d0a57c8-6f93-425d-9eb0-83a45a401277&client_secret=pgvJoQkjMGp+5Ti4Ny_l*rx+P7NqMgW7&grant_type=client_credentials&resource=https://graph.microsoft.com";
-
-            // 將轉為 string 的 json 依編碼並指定 content type 存為 httpcontent
-            HttpContent contentPost = new StringContent(body, Encoding.UTF8, "application/x-www-form-urlencoded");
-            // 發出 post 並取得結果
-            HttpResponseMessage response = client.PostAsync("https://login.microsoftonline.com/WangHsuan.onmicrosoft.com/oauth2/token", contentPost).GetAwaiter().GetResult();
-
-            return response;
-
-            //Second Method---------------------------------------------------------------------------
-
-            //string targetUrl = "https://login.microsoftonline.com/WangHsuan.onmicrosoft.com/oauth2/v2.0/token";
-            //string _client_id = "7d0a57c8-6f93-425d-9eb0-83a45a401277";
-            //string _client_secert = "pgvJoQkjMGp+5Ti4Ny_l*rx+P7NqMgW7";
-            //string body = $"client_id={_client_id} & client_secret={_client_secert} & grant_type = client_credentials & resource = https://graph.microsoft.com ";
-            //byte[] postData = Encoding.UTF8.GetBytes(body);
-
-            //HttpWebRequest request = WebRequest.Create(targetUrl) as HttpWebRequest;
-            //request.Method = "POST";
-            //request.ContentType = "multipart/form-data";
-            //request.Timeout = 30000;
-            //request.ContentLength = postData.Length;
-            //// 寫入 Post Body Message 資料流
-            //using (Stream st = request.GetRequestStream())
-            //{
-            //    st.Write(postData, 0, postData.Length);
-            //}
-
-            //string result = "";
-            //// 取得回應資料
-            //using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-            //{
-            //    using (StreamReader sr = new StreamReader(response.GetResponseStream()))
-            //    {
-            //        result = sr.ReadToEnd();
-            //    }
-            //}
-
-            //return result;
-
-            //Third-----------------------------------------------------
-
-
-            //// Create a request using a URL that can receive a post.   
-            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://login.microsoftonline.com/WangHsuan.onmicrosoft.com/oauth2/v2.0/token");
-            //// Set the Method property of the request to POST.  
-            //request.Method = "POST";
-
-            //// Create POST data and convert it to a byte array. 
-            //string _client_id = "7d0a57c8-6f93-425d-9eb0-83a45a401277";
-            //string _client_secert = "pgvJoQkjMGp+5Ti4Ny_l*rx+P7NqMgW7";
-            //string postData = $@"client_id={_client_id}&client_secret={_client_secert}&grant_type=client_credentials&scope=https://graph.microsoft.com/.default";
-            //byte[] byteArray = Encoding.UTF8.GetBytes(postData);
-
-            //// Set the ContentType property of the WebRequest.  
-            //request.ContentType = "application/x-www-form-urlencoded";
-            //// Set the ContentLength property of the WebRequest.  
-            //request.ContentLength = byteArray.Length;
-
-            //// Get the request stream.  
-            //Stream dataStream = request.GetRequestStream();
-            //// Write the data to the request stream.  
-            //dataStream.Write(byteArray, 0, byteArray.Length);
-            //// Close the Stream object.  
-            //dataStream.Close();
-
-            //// Get the response.  
-
-            //WebResponse response = request.GetResponse();
-
-
-            ////catch (WebException e)
-            ////{
-            ////    if (e.Status == WebExceptionStatus.ProtocolError) response = (HttpWebResponse)e.Response;
-            ////    else return false;
-            ////}
-            ////catch (Exception)
-            ////{
-            ////    if (response != null) response.Close();
-            ////    return false;
-            ////}
-            //return response;
-
-            //fourth---------------------------------------------------------
-
-
-
-        }
-
         public ToDoController(TodoContext context) {
 
             
             _context = context;
-            if (_context.TodoItems.Count() == 0) {
-                _context.TodoItems.Add(new TodoItem { AccessToken = getAccessToken().ToString()});
+            
+
+            // Create a request using a URL that can receive a post.   
+            WebRequest request = WebRequest.Create("https://login.microsoftonline.com/WangHsuan.onmicrosoft.com/oauth2/token");
+            // Set the Method property of the request to POST.  
+            request.Method = "POST";
+
+            // Create POST data and convert it to a byte array.  
+            string postData = "client_id=7d0a57c8-6f93-425d-9eb0-83a45a401277 & client_secret=bXW4wqCkIX4*2N3rCD*NMpwrttBKxy-.& grant_type=client_credentials & resource=https://graph.microsoft.com";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            // Set the ContentType property of the WebRequest.  
+            request.ContentType = "application/x-www-form-urlencoded";
+            // Set the ContentLength property of the WebRequest.  
+            request.ContentLength = byteArray.Length;
+
+            // Get the request stream.  
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.  
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.  
+            dataStream.Close();
+
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            
+
+            // Get the stream containing content returned by the server.  
+            // The using block ensures the stream is automatically closed.
+            using (dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.  
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.  
+                string responseFromServer = reader.ReadToEnd();
+
+                var hsuanJson = JObject.Parse(responseFromServer);
+                // Display the content.  
+                _context.TodoItems.Add(new TodoItem { AccessToken = hsuanJson["access_token"].ToString() });
                 _context.SaveChanges();
+                
             }
 
-            
+            // Close the response.  
+            response.Close();
+
+
         }
 
         
--- APIbooking/Models/TodoItem.cs
+++ APIbooking/Models/TodoItem.cs
@@ -9,14 +9,7 @@ namespace APIbooking.Models
     {
         public long Id { get; set; }
         public string AccessToken { get; set; }
-        public bool IsComplete { get; set; }
-
-        public string client_id { get; set; }
-        public string client_sercet { get; set; }
-
-        public string resource { get; set; }
-
-        public string grent_type { get; set; }
+      
 
     }
 }
